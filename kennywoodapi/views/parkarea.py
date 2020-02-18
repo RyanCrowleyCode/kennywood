03b730e4ce2dbe122516d3827d71309855d81d85
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-18 10:33:08 -0600

    API is working to handle requests for get one, get all, create, update, and delete for parkarea

--- kennywoodapi/views/parkarea.py
+++ kennywoodapi/views/parkarea.py
@@ -1,8 +1,9 @@
-"""View module for handling requests about park areas"""
+"""Park Areas for Kennywood Amusement Park"""
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers, status
+from rest_framework import serializers
+from rest_framework import status
 from kennywoodapi.models import ParkArea
 
 class ParkAreaSerializer(serializers.HyperlinkedModelSerializer):
@@ -61,7 +62,7 @@ class ParkAreas(ViewSet):
             Reponse -- JSON serialized ParkArea instance
         """
         newarea = ParkArea()
-        newarea = request.data["name"]
+        newarea.name = request.data["name"]
         newarea.theme = request.data["theme"]
         newarea.save()
 
@@ -71,3 +72,36 @@ class ParkAreas(ViewSet):
         )
 
         return Response(serializer.data)
+
+    # EDIT one
+    def update(self, request, pk=None):
+        """Handle PUT requests for a park area
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        area = ParkArea.objects.get(pk=pk)
+        area.name = request.data["name"]
+        area.theme = request.data["theme"]
+        area.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    # DELETE one
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single park area
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            area = ParkArea.objects.get(pk=pk)
+            area.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except ParkArea.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
